beaf6fdfdfde2bdaaa58770f0b715fbe268691d3
malayvatsa <24051181@kiit.ac.in>
2025-12-11 00:33:50 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Taking input from the user:");
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = 5;
+        int sum1 = num1 + num2 + num3;
+        // System.out.println(sum1);
+        Scanner jc = new Scanner(System.in);}
+}
